3a43f4c1d56300978d776a3dd67768b848045318
plengqui <per@lengquist.se>
2018-05-06 21:28:39 +0200

    hej

--- test.py
+++ test.py
@@ -16,21 +16,19 @@ temperature_sensor = 2
 
 while True:
 	# Read soil moisture
-        soil = analogRead(sensor)
-
-        # Get value from temperature sensor
-        [t,h]=[0,0]
-        [t,h] = dht(temperature_sensor,0)
-
-        print "Jordfukt=" , soil , "\t Luftfukt=" , h , "\t Temp=" , t 
+	soil = analogRead(sensor)
+	# Get value from temperature sensor
+	[t,h]=[0,0]
+	[t,h] = dht(temperature_sensor,0)
+	print("Jordfukt=" , soil , "\t Luftfukt=" , h , "\t Temp=" , t)
 
 	if soil<500:
 		time.sleep(1)
-		print "Watering!!"
-        	digitalWrite(pump,1)		# Send HIGH to switch on pump
-        	time.sleep(0.5)
-        	digitalWrite(pump,0)		# Send LOW to switch off pump
-	        time.sleep(4)
+		print("Watering!!")
+		digitalWrite(pump,1)		# Send HIGH to switch on pump
+		time.sleep(0.5)
+		digitalWrite(pump,0)		# Send LOW to switch off pump
+		time.sleep(4)
 
-        time.sleep(1)
+	time.sleep(1)
 
